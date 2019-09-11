9680ecc4649673ca411f9784ded1911bb30c0023
Eric Litwin <eric.litwin@pnmac.com>
2019-09-11 00:11:08 -0700

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateComponent from './components/CreateComponent';
+import EditComponent from './components/EditComponent';
+import IndexComponent from './components/IndexComponent';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Express Tutorial</h2>
+          <ul>
+          <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/create'}>Create</Link></li>
+            <li><Link to={'/index'}>List</Link></li>
+            <li><Link to={'/edit/:id'}>Edit</Link></li>
+          </ul>
+          <hr />
+          <Switch>
+              <Route exact path='/create' component={CreateComponent} />
+              <Route path='/edit/:id' component={EditComponent} />
+              <Route path='/index' component={IndexComponent} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
